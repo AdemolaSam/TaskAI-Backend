684e011a92b38cc252e45894ee0135360e02428f
AdemolaSam <oduntanade2721@gmail.com>
2024-06-15 11:24:10 -0400

    Feat: email setup

---
+++ src/services/email.service.js
@@ -0,0 +1,45 @@
+import nodemailer from 'nodemailer'
+import {
+    otpMailTemp,
+    projectCreationEmailTemp,
+    welcomeMailTemp
+} from "../utils/emailTemplates.js"
+
+const transporter = nodemailer.createTransport({
+    host: '',
+    port: '',
+    auth: {
+        user: '',
+        pass: ''
+    }
+})
+
+const mailSend = async (sender, receiver, subject, html, text) => {
+    const emailInfo = await transporter.sendMail({
+        from: '',
+        to: '',
+        subject: '',
+        html: '',
+        text: '',
+    })
+    
+    console.log(`Message sent to: ${emailInfo.messageId}`)
+}
+
+mailSend().catch(err => console.log(err.message))
+
+export const welcomeMail = async(sender, receiver, subject, html) => {
+    await mailSend(sender, receiver, subject, html=welcomeMailTemp)
+}
+
+
+export const sendOtp = async(sender, receiver, subject, html) => {
+    await mailSend(sender, receiver, subject, html=otpMailTemp)
+}
+
+export const projectCreationEmail = async(sender, receiver, subject, html) => {
+    await mailSend(sender, receiver, subject, html=projectCreationEmailTemp)
+}
+
+
+export default mailSend
---
+++ src/utils/emailTemplates.js
@@ -0,0 +1,33 @@
+export const welcomeMailTemp= (username) => {
+    return `
+        <div style="margin: auto; width: 100%; background: grey; ">
+            <h1>Welcome aboard ${username}</h1>
+            <p>Manage your projects efficiently with TaskMe</p>
+            <p>Take advantage of the integrated AI in Taskme to breakdown and manage your tasks</p>
+            <p>Enjoy Awesome deals and impeccable service.<p/>
+        </div>    
+    `
+}
+
+export const otpMailTemp = (otp)=> {
+    return  `
+    <center>
+        <p>Verify your account with this One Time Password. </p>
+        <p>It expires in 1 hour</p>
+        <b>${otp}</b>
+    </center>
+`
+   
+}
+
+
+
+export const projectCreationEmailTemp = (projectName) => {
+    return `
+    <div style="margin: auto; width: 100%; background: grey; ">
+        <p>You have successfully created a new project</p>
+        <p>Project Name: ${projectName}</p>
+    </div>
+`   
+}
+
