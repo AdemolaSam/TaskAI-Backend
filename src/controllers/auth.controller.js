f3df18a9581e024f3d5595869d1b17dc6ae71fa5
AdemolaSam <oduntanade2721@gmail.com>
2024-06-15 15:08:13 -0400

    Feat: Implemented Logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -37,7 +37,31 @@ export const login = async (req, res) => {
                 maxAge: 24 * 60 * 60 * 1000 //24 hours
             }
         )
+        userData.password = ''
+        return res.status(httpStatus.OK).json(userData)
     } catch (error) {
-        
+        if(error instanceof AppError){
+            return res.status(httpStatus.BAD_REQUEST).json({
+                message: error.message
+            })
+        }
+        return res.status(httpStatus.BAD_REQUEST).json({
+            message: error.message
+        })
+    }
+}
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie('accessToken')
+        return res.status(httpStatus.OK).json({
+            message: "You have successfully logged out"
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+            message: error.message,
+            error: "Internal Server Error"
+        })
     }
 }
