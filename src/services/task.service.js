ceba366eac31d52e19138154173014cb2281cd5f
AdemolaSam <oduntanade2721@gmail.com>
2024-06-15 16:02:52 -0400

    Feat: Implemented createTask, getTasks, and getTasksByProject services

--- src/services/task.service.js
+++ src/services/task.service.js
@@ -1,1 +1,22 @@
 import Task from "../models/Task.model";
+
+export const createTask = async(taskObj) => {
+    const newTask = await Task.create(taskObj)
+    return newTask
+}
+
+
+export const getTaskById = async(taskId) => {
+    const task = await Task.findByPk(taskId)
+    return task
+}
+
+export const getTasksByProject = async (projectId) => {
+    const tasks = await Task.findAll({
+        where: {
+            projectId: projectId
+        }
+    })
+
+    return tasks
+}
