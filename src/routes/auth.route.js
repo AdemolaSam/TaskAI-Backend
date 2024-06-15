5dc9fe6c96f265dd32052f23167dc535e4ef6aae
AdemolaSam <oduntanade2721@gmail.com>
2024-06-15 15:52:02 -0400

    Feat: Auth route mapped to auth Controllers

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,19 @@
 import { Router } from "express"
+import {
+    register,
+    login,
+    logout
+} from "../controllers/auth.controller.js"
 
 const router = Router()
 
 router.get('/', (req, res) => {
     return res.send("Hello world")
 })
+
+router.post('/', register)
+router.post('/login', login)
+router.post('/logout', logout)
 
 
 export default router
