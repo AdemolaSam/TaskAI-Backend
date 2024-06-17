4299b415689e1f229f4f50b3b9c53b58a4ae609d
AdemolaSam <oduntanade2721@gmail.com>
2024-06-16 21:53:02 -0400

    Feat: Get Project By Id

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -1,5 +1,5 @@
 import httpStatus from "http-status"
-import { createProject } from "../services/project.service.js"
+import { createProject, getProjectById } from "../services/project.service.js"
 import { AppError } from "../middlewares/error.js"
 
 export const createNewProject = async(req, res) => {
@@ -13,6 +13,23 @@ export const createNewProject = async(req, res) => {
             })
         }
 
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+            message: error.message,
+            error: "Internal Server Error"
+        })
+    }
+}
+
+export const fetchProjectById = async(req, res) => {
+    try {
+        const project = await getProjectById(req.params.projectId)
+        return res.status(httpStatus.OK).json(project)
+    } catch (error) {
+        if(error instanceof AppError){
+            return res.status(httpStatus.BAD_REQUEST).json({
+                message: error.message
+            })
+        }
         return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
             message: error.message,
             error: "Internal Server Error"
